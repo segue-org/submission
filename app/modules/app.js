15c8ec52e8347addbe2f75efdc9be43207dcdf6d
Felipe Mobus <fmobus@gmail.com>
2014-12-04 00:11:14 -0200

    applying IIFE form for app.js
    read more at http://benalman.com/news/2010/11/immediately-invoked-function-expression/

--- app/modules/app.js
+++ app/modules/app.js
@@ -1,22 +1,24 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular
-  .module('templates', []);
+  angular
+    .module('templates', []);
 
-angular
-  .module('segue.submission',[
-    'templates',
-    'ui.router',
-    'ui.router.compat',
-  ])
+  angular
+    .module('segue.submission',[
+      'templates',
+      'ui.router',
+      'ui.router.compat',
+    ])
 
-  .controller('SubmissionCtrl', function($scope) {
-    $scope.$on('$stateChangeSuccess', function(event, newState) {
-      $scope.topState = newState.name.split('.')[0];
-      $scope.state    = newState;
+    .controller('SubmissionCtrl', function($scope) {
+      $scope.$on('$stateChangeSuccess', function(event, newState) {
+        $scope.topState = newState.name.split('.')[0];
+        $scope.state    = newState;
+      });
+    })
+
+    .config(function($urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
     });
-  })
-
-  .config(function($urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-  });
+})();
