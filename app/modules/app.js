070344e9c06b72c2e1d86e11cc501b85eb42e28d
Felipe Mobus <fmobus@gmail.com>
2014-12-10 01:13:27 -0200

    added a language switcher to the interface

--- app/modules/app.js
+++ app/modules/app.js
@@ -6,22 +6,21 @@
 
   angular
     .module('segue.submission',[
-      'gettext',
       'templates',
       'ui.router',
       'ui.router.compat',
 
+      'segue.submission.locale',
       'segue.submission.proposal'
     ])
 
-    .controller('SubmissionCtrl', function($scope, gettextCatalog) {
+    .controller('SubmissionCtrl', function($scope, Locale) {
       $scope.$on('$stateChangeSuccess', function(event, newState) {
         $scope.topState = newState.name.split('.')[0];
         $scope.state    = newState;
       });
-
-      gettextCatalog.setCurrentLanguage('en');
-      gettextCatalog.loadRemote('/public/translations/messages.en.json');
+      $scope.languages = Locale.languages();
+      $scope.switchLanguage = Locale.switchTo;
     })
 
     .config(function($urlRouterProvider) {
