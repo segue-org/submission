631069583219638a9e10d3a2c81a564876a9f80e
Felipe Mobus <fmobus@gmail.com>
2015-02-03 02:55:31 -0200

    cleaning up logging

--- app/modules/directives.js
+++ app/modules/directives.js
@@ -6,9 +6,7 @@
     .directive("focusable", function($timeout) {
       return function(scope, elem, attr) {
         var myName = attr.focusable || attr.ngModel;
-        console.log('building', myName);
         scope.$on('focus-on', function(e, name) {
-          console.log(name, myName);
           if(name === myName) {
             $timeout(function() {
               elem[0].focus();
@@ -20,7 +18,6 @@
     .factory('focusOn', function ($rootScope, $timeout) {
       return function(name, timeout) {
         $timeout(function (){
-          console.log('signaling', name);
           $rootScope.$broadcast('focus-on', name);
         }, timeout || 100);
       };
