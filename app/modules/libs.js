5950a47b578d38b43b37328576a18466bdba67e8
Daniel Wolff <daniel@rapido.net.br>
2015-03-18 20:43:43 -0300

    added filter to show brazilian currency properly

--- app/modules/libs.js
+++ app/modules/libs.js
@@ -24,5 +24,50 @@
           return deferred.promise;
         }
       };
-    });
+    })
+    .filter('realbrasileiro', function() {
+      return function(input) {
+        function formatReal( int ){
+          var tmp = int+'';
+          var res = tmp.replace('.','');
+          tmp = res.replace(',','');
+          var neg = false;
+          if(tmp.indexOf('-') === 0){
+            neg = true;
+            tmp = tmp.replace('-','');
+          }
+          if(tmp.length === 1) {
+            tmp = '0'+tmp;
+          }
+          tmp = tmp.replace(/([0-9]{2})$/g, ',$1');
+          if( tmp.length > 6){
+            tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2');
+          }
+          if( tmp.length > 9){
+            tmp = tmp.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,'.$1.$2,$3');
+          }
+          if( tmp.length > 12){
+            tmp = tmp.replace(/([0-9]{3}).([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,'.$1.$2.$3,$4');
+          }
+          if(tmp.indexOf('.') === 0){
+            tmp = tmp.replace('.','');
+          }
+          if(tmp.indexOf(',') === 0){
+            tmp = tmp.replace(',','0,');
+          }
+          return (neg ? '-'+tmp : tmp);
+        }
+        
+        return 'R$ ' + formatReal(input);
+      };
+    })
+    .directive('watch', function() {
+      return function(scope, elem, attr) {
+        elem.on('change', function(ev) {
+          scope.$emit(attr.watch, elem);
+        });
+      };
+    })
+
 })();
+
