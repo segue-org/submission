ba0b34154bc8c0e626c00b6197ee67fe64e2026d
Daniel Wolff <daniel@rapido.net.br>
2015-04-22 14:54:21 -0300

    added group purchase and modifications on the pay method

--- app/modules/Purchase/service.js
+++ app/modules/Purchase/service.js
@@ -15,9 +15,13 @@
         return Restangular.all('products/caravan/' + caravan_hash).getList();
       };
       
+      extensions.getCorporateList = function() {
+        return Restangular.all('products/corporate').getList();
+      };
+      
       return _.extend(service, extensions);
     })
-    .factory('Product', function(Restangular) {
+    .factory('Product', function(Restangular, $q, Validator) {
       var service = Restangular.service('products') ;
       var extensions = {};
 
@@ -25,6 +29,16 @@
         return function(buyer_data) {
           var product = service.one(product_id);
           return product.post('purchase', buyer_data);
+        };
+      };
+
+      extensions.groupPurchase = function(product_id, newInvites) {
+        return function(buyer_data) {
+          return $q.all(newInvites.map(function(invite) {
+            return Validator.validate(invite, 'corporates/new_invite')
+                            .then(function() { buyer_data.post('invites', invite); })
+                            .catch(function() { console.log(arguments); });
+          }));
         };
       };
 
@@ -46,10 +60,20 @@
       extensions.saveIt = function(object) {
         return object.save();
       };
-      extensions.pay = function(method) {
+      extensions.pay = function(method, invites, product) {
+        return function(purchaseObject) {
+          var payload = purchaseObject;
+          var purchase = service.one(purchaseObject.id);
+          if (!_.isUndefined(invites)) {
+            payload.amount = product.price * invites.length;
+          }
+          return purchase.post('pay/' + method, payload);
+        };
+      };
+      extensions.payGroupPurchase = function(method) {
         return function(purchaseObject) {
           var purchase = service.one(purchaseObject.id);
-          return purchase.post('pay/' + method);
+          return purchase.post('group_pay/' + method);
         };
       };
       extensions.followPaymentInstructions = function(response) {
