293c147387be3fd1cbf5d0c2cfad8584ec575022
Daniel Wolff <daniel@rapido.net.br>
2015-04-27 05:21:22 -0300

    corrected and refactored purchase service

--- app/modules/Purchase/service.js
+++ app/modules/Purchase/service.js
@@ -10,15 +10,15 @@
     .factory('Products', function(Restangular) {
       var service = Restangular.service('products');
       var extensions = {};
-      
+
       extensions.getCaravanList = function(caravan_hash) {
         return Restangular.all('products/caravan/' + caravan_hash).getList();
       };
-      
+
       extensions.getCorporateList = function() {
         return Restangular.all('products/corporate').getList();
       };
-      
+
       return _.extend(service, extensions);
     })
     .factory('Product', function(Restangular, $q, Validator) {
@@ -29,16 +29,6 @@
         return function(buyer_data) {
           var product = service.one(product_id);
           return product.post('purchase', buyer_data);
-        };
-      };
-
-      extensions.groupPurchase = function(product_id, newInvites) {
-        return function(buyer_data) {
-          return $q.all(newInvites.map(function(invite) {
-            return Validator.validate(invite, 'corporates/new_invite')
-                            .then(function() { buyer_data.post('invites', invite); })
-                            .catch(function() { console.log(arguments); });
-          }));
         };
       };
 
@@ -60,20 +50,10 @@
       extensions.saveIt = function(object) {
         return object.save();
       };
-      extensions.pay = function(method, invites, product) {
-        return function(purchaseObject) {
-          var payload = purchaseObject;
-          var purchase = service.one(purchaseObject.id);
-          if (!_.isUndefined(invites)) {
-            payload.amount = product.price * invites.length;
-          }
-          return purchase.post('pay/' + method, payload);
-        };
-      };
-      extensions.payGroupPurchase = function(method) {
+      extensions.pay = function(method) {
         return function(purchaseObject) {
           var purchase = service.one(purchaseObject.id);
-          return purchase.post('group_pay/' + method);
+          return purchase.post('pay/' + method);
         };
       };
       extensions.followPaymentInstructions = function(response) {
